8d8fb16eadf88ed6fe6800e3c377d3093fd90001
alexdhljl <alexdhljl@gmail.com>
2025-10-20 22:13:11 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: "center", padding: "50px" }}>
-      <h1>EventFloX Landing Page</h1>
-      <p>Welcome to EventFloX AI Platform</p>
-    </main>
-  );
-}
